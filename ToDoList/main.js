c31a415b0d8abfdf9a6b6458ed4e43198a3e5067
LuYuanTing <luyuanting1026218@gmail.com>
2020-05-27 16:09:06 -0600

    everything work until toggle task

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,22 +1,29 @@
 function addList() {
-    // get parent element, which is ul
-    let item = document.getElementById('myUL')
-    // get the user input
+    //     // get parent element, which is ul
+    //     let item = document.getElementById('myUL')
+    //     // get the user input
+    //     let input = document.getElementById('myInput')
+    //     // add the to do list
+    //     item.innerHTML += `<li>${input.value}</li>`
+    //     input.value = ""
+
+    //     // attach all the to do list li with addEventListener
+    //     for (let oneLi of item.children) {
+
+    //         oneLi.addEventListener('click', () => {
+
+    //             oneLi.classList.add('checked')
+    //         })
+    //     }
+    // }
+
+    let parent = document.getElementById('myUL')
+    let liItem = document.createElement('li')
     let input = document.getElementById('myInput')
-    // add the to do list
-    item.innerHTML += `<li>${input.value}</li> `
-    input.value = ""
+    liItem.innerHTML += `<li>${input.value}</li>`
+    parent.append(liItem)
 
-    // attach all the to do list li with addEventListener
-    for (let oneList of item.children) {
+    function isCopmlete() {
+        let item = document.getElementById('myUL')
 
-        oneList.addEventListener('click', () => {
-            oneList.classList.add('checked')
-        })
     }
-}
-
-
-function isCopmlete() {
-
-}
