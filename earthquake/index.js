568d70caa5b2416de70d210319c1b0686312cd81
Yuan-Ting Lu <luyuanting1026218@gmail.com>
2020-06-23 21:57:54 -0600

    team activity 10 complete

--- earthquake/index.js
+++ earthquake/index.js
@@ -1,16 +1,17 @@
 import { getJSON, getLocation } from './utilities.js';
+import QuakesControler from './QuakesController.js'
 
 async function myFunction() {
-    let baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
-
-    // baseUrl = await getLocation()
-    let location = await getLocation()
-    let latitude = location.coords.latitude
-    let longitude = location.coords.longitude
-    console.log(latitude)
-
-    baseUrl += "&latitude=" + latitude + "&longitude=" + longitude + "&maxradiuskm=100"
-    console.log(baseUrl)
+    let baseUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 }
 
 myFunction()
+
+let myQuake = new QuakesControler('quakeList')
+const quakes = document.getElementById('quakeList').children
+
+for (let quake of quakes) {
+    quake.addEventListener('click', () => {
+        myQuake.getQuakeDetails(quake.id)
+    })
+}
